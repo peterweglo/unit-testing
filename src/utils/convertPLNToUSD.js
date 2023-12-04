b2d8042476057d64b3a820401b64b7192c97534d
Piotr Węgłowski <weglowskipiotr@gmail.com>
2023-12-04 13:44:21 +0100

    Add tests

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,18 @@
+export const convertPLNToUSD = (PLN) => {
+  const PLNtoUSD = PLN / 3.5;
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  });
+
+  if (typeof PLN === 'string' || PLN === undefined) {
+    return NaN;
+  } else if (PLN < 0) {
+    return '$0.00';
+  } else if (typeof PLN !== 'string' && typeof PLN !== 'number') {
+    return 'Error';
+  } else {
+    return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+  }
+};
